23a2b129922bdff8c51ced4ea0a69e9c4c18eb3c
Artsiom Sauchuk <artemsavchuk01@gmail.com>
2020-07-09 17:42:19 +0200

    Code from 'master' is moved to 'create-react-app' branch

--- src/Canvas/index.js
+++ src/Canvas/index.js
@@ -1,66 +1,17 @@
 import React from 'react';
 import * as THREE from 'three';
+import initialization from './init';
 import './Canvas.css';
 
 class Canvas extends React.Component {
-    
-    constructor(props) {
-        super(props);
-        
-        const fov = 40;
-        const aspect = 2;  // the canvas default
-        const near = 0.1;
-        const far = 100;
-        const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
 
-        camera.position.z = 20;
-        
-        const scene = new THREE.Scene();
-        scene.background = new THREE.Color(0xAAAAAA);
-
-        const boxWidth = 1;
-        const boxHeight = 1;
-        const boxDepth = 1;
-        const geometry = new THREE.BoxGeometry(boxWidth, boxHeight, boxDepth);
-        
-        const width = 10.0;  
-        const height = 10.0;  
-        const depth = 10.0;
-        const objGeometry = new THREE.CircleBufferGeometry(10, 30);
-        const objMaterial = new THREE.MeshPhongMaterial({ color: 0x44aa88 });
-        const obj = new THREE.Mesh(objGeometry, objMaterial);
-        obj.position.z = -20;
-
-        this.obj = obj;
-
-        {
-            const color = 0xFFFFFF;
-            const intensity = 1;
-            const light = new THREE.DirectionalLight(color, intensity);
-            light.position.set(-1, 2, 4);
-            scene.add(light);
-        }
-        
-        this.camera = camera;
-        this.scene = scene;
-        this.isActive = true;
-
-        this.cubes = [
-            makeCubeInstance(geometry, 0x44aa88,  0),
-            makeCubeInstance(geometry, 0x8844aa, -2),
-            makeCubeInstance(geometry, 0xaa8844,  2),
-        ];
-        
-        // this.cubes.forEach(cube => scene.add(cube))
-        scene.add(obj);
-
-    }
     componentDidMount() {
-        const canvas = document.getElementById("canvas");
-        this.renderer = new THREE.WebGLRenderer({ canvas });
+        // const canvas = document.getElementById("canvas");
+        // this.renderer = new THREE.WebGLRenderer({ canvas });
 
-        this.renderer.render(this.scene, this.camera);
-        window.requestAnimationFrame(() => this.renderUpdate());
+        // this.renderer.render(this.scene, this.camera);
+        // window.requestAnimationFrame(() => this.renderUpdate());
+        initialization(this);
         
     }
     componentWillUnmount() {
---
+++ src/Canvas/init.js
@@ -0,0 +1,344 @@
+import * as THREE from 'three';
+
+let dogLegFlag = false
+
+let leftDogArm;
+let rightDogArm;
+let leftDogLeg;
+let rightDogLeg;
+let dogGroup;
+let time = 0;
+let legForwardFlag = true;
+
+const fov = 45;
+const near = 0.001;
+const far = 100000;
+const camera = new THREE.PerspectiveCamera(fov, window.innerWidth / window.innerHeight, near, far);
+
+let scene = new THREE.Scene();
+
+
+let humanGroup = new THREE.Object3D();
+let humanUpperGroup = new THREE.Object3D();
+let renderer;
+
+function initialization(component) {
+    
+
+    camera.up.set(0, 1, 0);
+    camera.lookAt(0, 0, 0);
+
+    createHumanoid();
+    createDoggo();
+    const canvas = document.getElementById("canvas");
+    renderer = new THREE.WebGLRenderer({ canvas });
+
+    renderer.render(scene, camera);
+    const mainLoop = () => {
+
+        if (resizeRendererToDisplaySize(renderer)) {
+            const canvas = renderer.domElement;
+            camera.aspect = canvas.clientWidth / canvas.clientHeight;
+            camera.updateProjectionMatrix();
+        }
+
+        if (time <= 20 && legForwardFlag) {
+
+            leftDogArm.rotation.z -= (Math.PI / 180);
+            rightDogArm.rotation.z += Math.PI / 180; 
+            rightDogLeg.rotation.z -= Math.PI / 180;
+
+            leftDogLeg.rotation.z += Math.PI / 180;
+
+            rightDogLeg.children[0].rotateZ(-Math.PI / 180);
+            leftDogLeg.children[0].rotateZ(-Math.PI / 180);
+
+            leftDogArm.children[0].rotateZ(Math.PI / 180);
+            rightDogArm.children[0].rotateZ(-Math.PI / 180);
+
+           (time === 20) ? (legForwardFlag = false) : time += 1;
+        }
+        else {
+            //alert(time); 
+            leftDogArm.rotation.z += Math.PI /180;                
+            rightDogArm.rotation.z -= Math.PI / 180;
+
+            rightDogLeg.rotation.z += Math.PI / 180; 
+            leftDogLeg.rotation.z -= Math.PI / 180;
+
+            rightDogLeg.children[0].rotateZ(Math.PI / 180);
+            leftDogLeg.children[0].rotateZ(Math.PI / 180);
+
+            leftDogArm.children[0].rotateZ(Math.PI / 180);
+            rightDogArm.children[0].rotateZ(Math.PI / 180);
+
+
+
+            (time === 0) ? legForwardFlag = true : time -= 1;
+
+        }
+
+           requestAnimationFrame(mainLoop)
+
+           renderer.render(scene, camera);
+
+    }
+    mainLoop();
+}
+
+
+function createArm() {
+
+    let armMaterial;
+
+    let lowerArmCylinder;
+    let upperArmCylinder;
+    let upperArmCylinderGeometry;
+    let lowerArmCylinderGeometry;
+
+    if (dogLegFlag) {
+        armMaterial = new THREE.MeshPhongMaterial({ emissive: 0xD2691E });
+
+        upperArmCylinderGeometry = new THREE.CylinderGeometry(0.06, 0.06, 0.35, 0.005);
+        upperArmCylinder = new THREE.Mesh(upperArmCylinderGeometry, armMaterial);
+
+        lowerArmCylinderGeometry = new THREE.CylinderGeometry(0.06, 0.06, 0.35, 0.005);
+        lowerArmCylinder = new THREE.Mesh(lowerArmCylinderGeometry, armMaterial);
+
+    }
+
+    else {
+        const armMaterial2 = new THREE.MeshBasicMaterial({ color: 0xcfffff });
+
+        armMaterial = new THREE.MeshBasicMaterial({ color: 0xD2691E });
+        upperArmCylinderGeometry = new THREE.CylinderGeometry(0.06, 0.06, 0.35, 0.005);
+        upperArmCylinder = new THREE.Mesh(upperArmCylinderGeometry, armMaterial);
+
+        lowerArmCylinderGeometry = new THREE.CylinderGeometry(0.06, 0.06, 0.35, 0.005);
+        lowerArmCylinder = new THREE.Mesh(lowerArmCylinderGeometry, armMaterial2);
+    }
+
+    lowerArmCylinder.translateY(-0.35);
+    upperArmCylinder.add(lowerArmCylinder);
+    return upperArmCylinder;
+
+}
+
+function createLegs() {
+
+    let upperLegCylinderGeometry;
+    let upperLegCylinder;
+
+    let lowerLegCylinderGeometry;
+    let lowerLegCylinder;
+    let legMaterial = new THREE.MeshPhongMaterial({ emissive: 0xc11f0f });
+    let legMaterial2 = new THREE.MeshBasicMaterial({ emissive: 0xc11fff });
+
+
+    upperLegCylinderGeometry = new THREE.CylinderGeometry(0.07, 0.07, 0.95, 0.005);
+    upperLegCylinder = new THREE.Mesh(upperLegCylinderGeometry, legMaterial);
+
+    lowerLegCylinderGeometry = new THREE.CylinderGeometry(0.07, 0.07, 0.25, 0.005);
+    lowerLegCylinder = new THREE.Mesh(lowerLegCylinderGeometry, legMaterial2);
+
+    lowerLegCylinder.translateY(-0.6);
+    upperLegCylinder.add(lowerLegCylinder);
+
+    return upperLegCylinder;
+
+}
+
+
+
+function createHumanoid() {
+
+    dogLegFlag = false;
+
+    let torsoCylinderGeometry = new THREE.CylinderGeometry(0.23, 0.23, 0.95, 0.35);
+    const torsoCylinderMaterial = new THREE.MeshBasicMaterial({ color: 0xcf1ff6 });
+
+    let torsoCylinder = new THREE.Mesh(torsoCylinderGeometry, torsoCylinderMaterial);
+
+    const neckCylinderGeometry = new THREE.CylinderGeometry(0.07, 0.07, 0.18, 0.05);
+    const neckCylinderMaterial = new THREE.MeshBasicMaterial({ color: 0xcf1f1f });
+
+    const neckCylinder = new THREE.Mesh(neckCylinderGeometry, neckCylinderMaterial);
+
+    torsoCylinder = new THREE.Mesh(torsoCylinderGeometry, torsoCylinderMaterial);
+    humanGroup.position.set(-2, 0, -4);
+    torsoCylinder.add(neckCylinder);
+
+    humanUpperGroup.add(torsoCylinder);
+
+    const sphereGeometry = new THREE.SphereBufferGeometry(
+        0.18, 20, 20);
+
+    const headMaterial = new THREE.MeshPhongMaterial({ emissive: 0xFFFF00 });
+    const headMesh = new THREE.Mesh(sphereGeometry, headMaterial);
+    neckCylinder.add(headMesh);
+
+    headMesh.translateY(0.27);
+    neckCylinder.translateY(0.55);
+
+    const leftArms = createArm();
+    const rightArms = createArm();
+
+    const leftLegs = createLegs();
+    const rightLegs = createLegs();
+
+    rightArms.translateX(0.29);
+    rightArms.translateY(0.25);
+
+    leftArms.translateX(-0.27);
+    leftArms.translateY(0.25);
+
+    rightLegs.translateX(0.08);
+    rightLegs.translateY(-0.55);
+
+    leftLegs.translateX(-0.12);
+    leftLegs.translateY(-0.55);
+
+    torsoCylinder.add(rightArms);
+    torsoCylinder.add(leftArms);
+    humanUpperGroup.add(leftLegs);
+    humanUpperGroup.add(rightLegs);
+    //humanUpperGroup.translateY(-0.2); when you will use the decreasing part of the upper body you will use this in the animation
+    //humanUpperGroup.rotateZ(radians); //when the human will get the stick from the ground he will use this to lay down 
+    humanGroup.add(humanUpperGroup);
+   
+    //humanGroup.scale.set(1,1,1.2); 
+    scene.add(humanGroup);
+
+}
+
+
+
+function createDoggo() {
+
+    dogGroup = new THREE.Object3D();
+    dogLegFlag = true;
+
+    const headGeometry = new THREE.SphereBufferGeometry(
+        0.25, 20, 20);
+    const sphereGeometry = new THREE.SphereBufferGeometry(
+        0.25, 20, 40);
+
+    const eyesGeometry = new THREE.SphereBufferGeometry(
+        0.045, 50, 50);
+    const eyesMaterial = new THREE.MeshBasicMaterial({ color: 0xdad9ff});
+
+    const eye1 = new THREE.Mesh(eyesGeometry, eyesMaterial);
+    const eye2 = new THREE.Mesh(eyesGeometry, eyesMaterial);
+
+    const noseMaterial = new THREE.MeshBasicMaterial({ color: 0xda2c43 });
+    const nose = new THREE.Mesh(eyesGeometry, noseMaterial);
+
+    dogGroup.position.set(1, 0, -4);
+
+    const earGeometry = new THREE.BoxGeometry(0.08, 0.3, 0.2);
+    const earMaterial = new THREE.MeshBasicMaterial({ color: 0x000000 });
+    const ear1 = new THREE.Mesh(earGeometry, earMaterial);
+    const ear2 = new THREE.Mesh(earGeometry, earMaterial);
+
+    ear1.translateZ(0.2);
+    ear2.translateZ(-0.2);
+                
+
+    const neckCylinderGeometry = new THREE.CylinderGeometry(0.15, 0.15, 0.05, 0.05);
+    const neckCylinderMaterial = new THREE.MeshBasicMaterial({ color: 0xffffff });
+
+    const neckCylinder = new THREE.Mesh(neckCylinderGeometry, neckCylinderMaterial);
+
+    const tailCylinderGeometry = new THREE.CylinderGeometry(0.23, .05, 0.05, 0.06);
+
+
+    const sphereGeometryTorso = sphereGeometry.scale(2.5, 1, 1);
+
+    const torsoMaterial = new THREE.MeshPhongMaterial({ emissive: 0xD2691E });
+    const torsoMesh = new THREE.Mesh(sphereGeometryTorso, torsoMaterial);
+
+    const headMesh = new THREE.Mesh(headGeometry, torsoMaterial);
+
+    leftDogLeg = createArm();
+    leftDogArm = createArm();
+
+    rightDogLeg = createArm();
+    rightDogArm = createArm();
+
+    const tailMesh = new THREE.Mesh(tailCylinderGeometry, torsoMaterial);
+    tailMesh.rotateZ(Math.PI / 7);
+
+    tailMesh.translateX(0.7);
+    tailMesh.translateY(-0.1);
+
+    headMesh.add(eye1);
+    headMesh.add(eye2);
+    headMesh.add(ear1);
+    headMesh.add(ear2);
+    headMesh.add(nose);
+
+    nose.translateX(-0.24);
+    nose.translateY(-0.05);
+    eye1.translateZ(0.15);
+    eye1.translateY(0.05)
+    eye1.translateX(-0.2);
+
+    eye2.translateZ(-0.15);
+    eye2.translateY(0.05);
+    eye2.translateX(-0.2);
+
+
+    neckCylinder.translateX(-0.5);
+    neckCylinder.translateY(0.2);
+    headMesh.translateY(0.3);
+    headMesh.rotateZ(-Math.PI / 7); //due to neck, also the head is rotated. with this it is reversed.
+    neckCylinder.rotateZ(Math.PI / 7);
+
+    rightDogArm.translateX(0.3);
+    rightDogArm.translateY(-0.3);
+    rightDogArm.translateZ(0.2);
+
+
+    leftDogArm.translateX(-0.3);
+    leftDogArm.translateY(-0.3);
+    leftDogArm.translateZ(-0.2);
+
+
+    rightDogLeg.translateX(0.3);
+    rightDogLeg.translateY(-0.3);
+    rightDogLeg.translateZ(0.2);
+
+
+    leftDogLeg.translateX(-0.3);
+    leftDogLeg.translateY(-0.3);
+    leftDogLeg.translateZ(-0.2);
+
+
+
+    torsoMesh.add(leftDogArm);
+    torsoMesh.add(leftDogLeg);
+    torsoMesh.add(rightDogArm);
+    torsoMesh.add(rightDogLeg);
+
+    torsoMesh.add(tailMesh);
+    torsoMesh.add(neckCylinder);
+    neckCylinder.add(headMesh);
+
+    dogGroup.add(torsoMesh);
+    scene.add(dogGroup);
+
+
+}
+
+function resizeRendererToDisplaySize(renderer) {
+    const canvas = renderer.domElement;
+    const width = canvas.clientWidth;
+    const height = canvas.clientHeight;
+    const needResize = canvas.width !== width || canvas.height !== height;
+    if (needResize) {
+      renderer.setSize(width, height, false);
+    }
+    return needResize;
+}
+
+export default initialization;
