d51ce65953429962765a0f2250c3c91a2350beb9
Artsiom Sauchuk <artemsavchuk01@gmail.com>
2020-07-12 15:00:58 +0200

    Added the light and floor

---
+++ src/constants.js
@@ -0,0 +1,9 @@
+import { Vector3 } from "three";
+
+export const GLOBAL_LIGHT_COLOR = 0xffffff;
+export const GLOBAL_LIGHT_INTENSITY = 0.7;
+export const GLOBAL_LIGHT_POSITION = new Vector3(40, 40, 0);
+export const GLOBAL_LIGHT_TARGET = new Vector3(0, 0, 0);
+
+export const AMBIENT_LIGHT_COLOR = 0xffffff;
+export const AMBIENT_LIGHT_INTENSITY = 0.3;
