4ff0944289bd1e1de4e448ada60a55400a1d4c60
berraksenses <berraksenses@gmail.com>
2020-07-26 01:37:02 +0300

    Updates

---
+++ precache-manifest.3a00756235a71a97662cd013d844d14a.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c633fb9cf1f7601c0e06692baf080651",
+    "url": "/finalProject/index.html"
+  },
+  {
+    "revision": "60a6681f53cc0ed5ae25",
+    "url": "/finalProject/static/css/main.e3d32c9d.chunk.css"
+  },
+  {
+    "revision": "b6b7e3dc6c5c6d7275df",
+    "url": "/finalProject/static/js/2.5eabf4e0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/finalProject/static/js/2.5eabf4e0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "60a6681f53cc0ed5ae25",
+    "url": "/finalProject/static/js/main.bab22afe.chunk.js"
+  },
+  {
+    "revision": "508bd483cdc72ca59b1c",
+    "url": "/finalProject/static/js/runtime-main.8dffc51e.js"
+  },
+  {
+    "revision": "11fdb98f3daa502e19a24e91d91ccbe9",
+    "url": "/finalProject/static/media/10450_Rectangular_Grass_Patch_v1_Diffuse.11fdb98f.jpeg"
+  },
+  {
+    "revision": "27d19274cf609012cc18eba41ebcbf99",
+    "url": "/finalProject/static/media/10450_Rectangular_Grass_Patch_v1_iterations-2.27d19274.mtl"
+  },
+  {
+    "revision": "972d22eb2adbea6d49e6deba81d3c857",
+    "url": "/finalProject/static/media/10450_Rectangular_Grass_Patch_v1_iterations-2.972d22eb.obj"
+  },
+  {
+    "revision": "bf6e2636b008172a9340c96c0e0c4cea",
+    "url": "/finalProject/static/media/Fence.bf6e2636.obj"
+  },
+  {
+    "revision": "1a7dd781df83f4293d5baac4d9efefee",
+    "url": "/finalProject/static/media/Thai_house.1a7dd781.obj"
+  },
+  {
+    "revision": "382176cdbe81e997d51d6d69d0987025",
+    "url": "/finalProject/static/media/bark.382176cd.png"
+  },
+  {
+    "revision": "358102cb0d99267b347eecf9514ef5db",
+    "url": "/finalProject/static/media/creme.358102cb.jpg"
+  },
+  {
+    "revision": "a3430e058e27c12ebf4d3bcb937cc06a",
+    "url": "/finalProject/static/media/grass.a3430e05.jpg"
+  },
+  {
+    "revision": "fb599f5d2506305ba3ed02a3979a413d",
+    "url": "/finalProject/static/media/leaf.fb599f5d.png"
+  },
+  {
+    "revision": "8397dda6f57796da8d71711b720246f4",
+    "url": "/finalProject/static/media/shirt.8397dda6.jpg"
+  },
+  {
+    "revision": "06eb0b4774cd42c6291c3f34aa6edecb",
+    "url": "/finalProject/static/media/skinTexture.06eb0b47.jpg"
+  },
+  {
+    "revision": "a6e38cb058827532d213de0a26f240ce",
+    "url": "/finalProject/static/media/tree2.a6e38cb0.obj"
+  }
+]);
